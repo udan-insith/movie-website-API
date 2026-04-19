c6d5b57b43d18e0113caf604a641f70b5719a241
udan-insith <udaninsith23613@gmail.com>
2026-04-19 09:00:32 +0530

    movie card added

--- Frontend/src/components/MovieCard.jsx
+++ Frontend/src/components/MovieCard.jsx
@@ -1,3 +1,18 @@
-function MovieCard() {}
+function MovieCard({ movie }) {
+  function onFavouriteClick() {
+    alert("clicked");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favourite-btn" onClick={onFavouriteClick}></button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default MovieCard;
