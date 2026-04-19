f1bd7ebdb26294ad31df695b483070ca194057a9
udan-insith <udaninsith23613@gmail.com>
2026-04-19 15:56:40 +0530

    updated code

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import MovieCard from "../components/MovieCard.jsx";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "1999" },
     { id: 3, title: "The Matrix", release_date: "1998" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = () => {
+    alert(searchQuery);
+  };
 
   return (
     <div className="home">
@@ -16,6 +21,8 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
