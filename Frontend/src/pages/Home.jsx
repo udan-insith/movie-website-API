746cc89b263cfbdeea119060557c1161fa3cb164
udan-insith <udaninsith23613@gmail.com>
2026-04-19 13:33:42 +0530

    update

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import MovieCard from "../components/MovieCard.jsx";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "1999" },
+    { id: 3, title: "The Matrix", release_date: "1998" },
+  ];
+
+  return (
+    <div className="home">
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
