4a2ba9e4a9904b5a25fbc861b961efb52304371c
udan-insith <udaninsith23613@gmail.com>
2026-04-19 11:45:12 +0530

    conditional rendering

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,10 +2,15 @@ import "./App.css";
 import MovieCard from "./components/MovieCard.jsx";
 
 function App() {
+  const movieNumber = 1;
+
   return (
     <>
-      <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
-      <MovieCard movie={{ title: "Joe's Film", release_date: "2025" }} />
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+      ) : (
+        <MovieCard movie={{ title: "Joe's Film", release_date: "2025" }} />
+      )}
     </>
   );
 }
