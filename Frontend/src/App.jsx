f8f7e4dce387217d5a967efd433d689b8b5cb1b5
udan-insith <udaninsith23613@gmail.com>
2026-04-19 13:50:41 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import MovieCard from "./components/MovieCard.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
   const movieNumber = 1;
 
   return (
     <>
-      {movieNumber === 1 ? (
+      <Home />
+      {/* {movieNumber === 1 ? (
         <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
       ) : (
         <MovieCard movie={{ title: "Joe's Film", release_date: "2025" }} />
-      )}
+      )} */}
     </>
   );
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,8 +7,21 @@ function Home() {
     { id: 3, title: "The Matrix", release_date: "1998" },
   ];
 
+  const handleSearch = () => {};
+
   return (
     <div className="home">
+      <form className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies....."
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
       <div className="movies-grid">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
