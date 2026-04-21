11597337167549cbe26d93dad9d9a4973984b401
udan-insith <udaninsith23613@gmail.com>
2026-04-21 13:04:32 +0530

    navbar done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
+      <NavBar />
 
       <main className="main-content">
         <Routes>
