623e79e205d4e6f077bee09b527386654cd1bae0
udan-insith <udaninsith23613@gmail.com>
2026-04-21 12:33:16 +0530

    updated states and urls and more

---
+++ Frontend/src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourite() {
+  return (
+    <div className="favourite-empty">
+      <h2>No Favourite Movies Yet</h2>
+      <p>Start adding to your favourites and they will appear here.</p>
+    </div>
+  );
+}
+
+export default Favourite;
