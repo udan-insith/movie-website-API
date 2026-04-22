f5c26cdd16701756360b3d044f8a704b5475399d
udan-insith <udaninsith23613@gmail.com>
2026-04-22 09:50:36 +0530

    use effects updated

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,29 @@
 import MovieCard from "../components/MovieCard.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { searchMovies, getPopularMovies } from "../services/api.js";
 import "../css/Home.css";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
