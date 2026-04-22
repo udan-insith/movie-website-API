9475ce27d06e24cdfd6b8d55163be47969dd63bc
udan-insith <udaninsith23613@gmail.com>
2026-04-22 10:19:37 +0530

    images updated

--- Frontend/src/components/MovieCard.jsx
+++ Frontend/src/components/MovieCard.jsx
@@ -8,7 +8,10 @@ function MovieCard({ movie }) {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
           <button className="favourite-btn" onClick={onFavouriteClick}>
             ♥
